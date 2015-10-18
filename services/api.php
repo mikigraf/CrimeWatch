737d3feaea9091a7a7f11501e98a61170a8c8efc
skai92 <skai92@users.noreply.github.com>
2015-10-18 07:36:31 +0200

    Added some icons etc.

---
+++ services/api.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_GET['lat'])) $lng = $_GET['lat'];
+if(isset($_GET['lng'])) $lat = $_GET['lng'];
+
+$db = new mysqli('localhost', 'jh2015', 'aesheiC3aidie3ku', 'jh2015');
+
+if (mysqli_connect_errno() == 0 && isset($lat) && isset($lng))
+{
+    $sql = 'SELECT *, (3959 * acos(cos(radians('.$lat.')) * cos(radians(lat)) * cos(radians(lng) - radians('.$lng.')) + sin(radians('.$lat.')) * sin(radians(lat)))) AS distance FROM crimes HAVING distance < 5 ORDER BY distance;';
+    $ergebnis = $db->query($sql);
+
+    $i = 0;
+    $num_rows = $ergebnis->num_rows;
+
+    echo '{"type": "FeatureCollection",
+           "features": [';
+    while ($zeile = $ergebnis->fetch_object())
+    {
+        echo '{
+		  "type": "Feature",
+		  "geometry": {
+			"type": "Point",
+                        "coordinates": ['.$zeile->lng.', '.$zeile->lat.']
+                  },
+                  "properties": {
+			"type": "'.$zeile->type.'",
+                        "city": "'.$zeile->city.'",
+			"details": "'.$zeile->details.'"
+		  }
+		}';
+       $i++;
+       if($i != $num_rows) echo ',';
+    }
+    echo ']}';
+
+    $ergebnis->close();
+}
+else
+{
+    echo 'Die Datenbank konnte nicht erreicht werden. Folgender Fehler trat auf: <strong>' .mysqli_connect_errno(). ' : ' .mysqli_connect_error(). '</strong>';
+}
+$db->close();
